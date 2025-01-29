c3532af0bde2678b2f198c3cb7f54596d0b25386
2826WaveRobotics <wavesoftware@waverobotics.com>
2025-01-28 21:52:55 -0600

    Start implementing climber

--- src/main/java/frc/robot/subsystems/arm/ArmIO.java
+++ src/main/java/frc/robot/subsystems/arm/ArmIO.java
@@ -5,6 +5,9 @@ import org.littletonrobotics.junction.AutoLog;
 public interface ArmIO {
     @AutoLog
     public static class ArmIOInputs {
+        /**
+         * The distance reported by a range finder pointing at the game piece, allowing us to sense its presence.
+         */
         public double gamePieceDistance = 0.0;
         public boolean elevatorLimitSwitchActive = false;
     }
---
+++ src/main/java/frc/robot/subsystems/climber/ClimberConstants.java
@@ -0,0 +1,12 @@
+package frc.robot.subsystems.climber;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+
+/**
+ * Constants related to the climber subsystem.
+ */
+public class ClimberConstants {
+    public static final int climberMotorId = /* TODO */ 36;
+    public static final Rotation2d climberZeroAngle = Rotation2d.fromDegrees(105.30);
+    public static final boolean invertClimber = false;
+}
