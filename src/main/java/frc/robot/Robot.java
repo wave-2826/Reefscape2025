0903cf72ccbb99c7812ea568a3cd2360c0ce8cd8
Glitch752 <xxGlitch752xx@gmail.com>
2025-03-17 10:38:21 -0500

    Improve NFC communication handling and add more metadata

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -9,6 +9,7 @@ import edu.wpi.first.wpilibj.Watchdog;
 import edu.wpi.first.wpilibj.simulation.DriverStationSim;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.subsystems.vision.VisionConstants;
 import frc.robot.util.LoggedTracer;
 import frc.robot.util.LoggedTunableSparkPID;
 import frc.robot.util.NTClientLogger;
@@ -46,11 +47,14 @@ public class Robot extends LoggedRobot {
 
     public Robot() {
         // Record metadata
+        Logger.recordMetadata("TuningMode", Boolean.toString(Constants.tuningMode));
+        Logger.recordMetadata("AprilTagLayout", VisionConstants.aprilTagLayout.toString());
         Logger.recordMetadata("ProjectName", BuildConstants.MAVEN_NAME);
         Logger.recordMetadata("BuildDate", BuildConstants.BUILD_DATE);
         Logger.recordMetadata("GitSHA", BuildConstants.GIT_SHA);
         Logger.recordMetadata("GitDate", BuildConstants.GIT_DATE);
         Logger.recordMetadata("GitBranch", BuildConstants.GIT_BRANCH);
+        
         switch(BuildConstants.DIRTY) {
             case 0:
                 Logger.recordMetadata("GitDirty", "All changes committed");
