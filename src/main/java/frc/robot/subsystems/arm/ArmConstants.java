a99961a3f38a82b8e2d55a021797161c2879706c
2826WaveRobotics <wavesoftware@waverobotics.com>
2025-03-20 17:12:03 -0500

    Update at 'Thu Mar 20 17:12:03 CDT 2025'

--- src/main/java/frc/robot/subsystems/arm/ArmConstants.java
+++ src/main/java/frc/robot/subsystems/arm/ArmConstants.java
@@ -117,7 +117,7 @@ public class ArmConstants {
         public static final int armWristMotorId = 53;
 
         /** The wrist absolute encoder zero offset, in radians. */
-        public static final double wristZeroOffset = 0.7912476;
+        public static final double wristZeroOffset = 0.8446950;
         /** The pitch absolute encoder zero offset, in radians. */
         public static final double pitchZeroOffset = 0.3618647;
 
